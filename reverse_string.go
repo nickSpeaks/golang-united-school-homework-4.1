36b1fdfa1ec98aced66129a71977fd086d2f8b61
nickSpeaks <andrejeu@gmail.com>
2022-05-23 17:28:55 +0400

    reverse string using for range

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	output = ""
+	for _, v := range input {
+		output = string(v) + output
+	}
 	return output
 }
